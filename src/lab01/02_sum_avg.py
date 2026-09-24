64ead9c1ab787a6128135d285433dd484e62b671
Amir <rushvarsscachal@gmail.com>
2026-09-24 23:31:37 +0300

    lab1

---
+++ src/lab01/02_sum_avg.py
@@ -0,0 +1,5 @@
+a = float(input().replace(',', '.'))
+b = float(input().replace(',', '.'))
+sum = round(a + b, 2)
+avg = round((a + b) / 2, 2)
+print(f"sum={sum}; avg={avg}")
---
+++ src/lab01/03_discount_vat.py
@@ -0,0 +1,11 @@
+price = float(input("Цена: "))
+discount = float(input("Скидка: "))
+vat = float(input("НДС: "))
+
+base = round(price * (1 - discount / 100),2)
+vat_amount = round(base * (vat / 100),2)
+total = round(base + vat_amount,2)
+
+print(f"База после скидки: {base} ₽")
+print(f"НДС: {vat_amount} ₽")
+print(f"Итого к оплате: {total} ₽")
